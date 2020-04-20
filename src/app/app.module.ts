2c72b11d3e91e10b753d7e3deaece8a22795be5f
SriLakshmi-Ganjam <srilakshmiganjam@gmail.com>
2020-04-20 13:41:20 +0530

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ContactUsComponent,
+    AboutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
